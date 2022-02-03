203598efdbf8d76cbceac99cb19266f0e85d2f32
KusukabeAsako <86934881+KusukabeAsako@users.noreply.github.com>
2022-02-02 22:36:11 -0800

    Add files via upload
    The config file is the file he said to make for constants and the like
    my page.py is assuming that when we create a page, we will specify the size of the objects that it will contain. Will need to be changed if we deem that not to be the case.

---
+++ page.py
@@ -0,0 +1,48 @@
+﻿import config
+import sys
+
+#assuming that every column is either a string or int of some predetermined max size
+class Page:
+
+    def __init__(self, size):
+        self.num_records = 0
+        self.data_size = size
+        self.data = bytearray(config.page_size)
+
+    def has_capacity(self): #haven't fully tested
+        if (config.page_size/self.data_size >= self.num_records):
+            return(True)
+        return(False)
+
+    def write(self, value):
+        type_ = type(value);
+        if (type_ == int):
+            temp = str(value)
+            tempArr = bytearray(temp)
+        elif (type_ == str):
+            tempArr = bytearray(value)
+        else:
+            print("Invalid Value Type")
+            return(False)
+        size = sys.getsizeof(tempArr)
+        print(size)
+        if (size > self.data_size+49): #no idea why but seems to return size of 49 + num characters
+            print("Value too Large")
+            return(False)
+        else:
+            count = len(tempArr)-1
+            while(count >= 0):
+                self.data[self.num_records*self.data_size + count] = tempArr[count]
+                count -= 1
+            self.num_records += 1
+            return(True)
+
+test = Page(int(5)) # can change size here
+print(test.data)
+i = 0
+while(i < 10):
+    print(test.write(1611)) #should work for any int or string within the size specified on line 40
+    i += 1
+print(test.data)
+
+
