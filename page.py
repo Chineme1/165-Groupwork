1210d811a9a0964d3175f99419d84384a4505d0f
KusukabeAsako <86934881+KusukabeAsako@users.noreply.github.com>
2022-02-07 23:09:56 -0800

    Add files via upload

--- page.py
+++ page.py
@@ -1,4 +1,4 @@
-from lstore.table import Table
+
 
 class Page:
 
@@ -22,7 +22,8 @@ class Page:
 
     def write(self, value):
         if self.has_capacity:
-            self.num_records[self.num_records*8 : self.num_records*8+7] = value
+            arr = value.to_bytes(8, 'big')
+            self.data.extend(arr)
             self.num_records += 1
         else:
             return False
@@ -30,44 +31,17 @@ class Page:
         self.num_records[position*8 : position*8+7] = value
         return(True)
 
-class PageRange:
-    def __init__(self,columns):
-        self.columns = columns
-        self.TP = 1
-        self.BP = 0
-        #TP1 = TP(columns) #necessary?
-        #BP1 = BP(columns) #necessary?
-        self.hold = [None*16]
-    
-    def I_TP(self,value):
-        x1 = Page()
-        self.hold.append(x1) #change to insert
-        self.TP +=1
-
-    def I_BP(self):
-        if self.has_capacity:
-            np= Page()
-            self.hold[self.BP]=np
-            self.BP +=1
-        else:
-            return False
-
-    def has_capacity(self):
-        if self.BP == 16:
-            return False
-        else:
-            return True
-    def read(self,position,column):
-        page = position//8192
-        position2 = position%8192
-        return(self.hold[page].read(position2))
     
 
 class BP:
     def __init__(self,columns):
         self.columns = columns
-        self.hold = [None* columns] #Physiical holder
+        self.hold = []#Physiical holder
         self.counter = 0
+        self.updates = 0
+        for i in range(0, self.columns+4):
+            x0 = Page()
+            self.hold.append(x0)
     
     
         #def I_PP(self):
@@ -79,16 +53,12 @@ class BP:
     #   if self.columns == columns
 
     def write(self,value, column):
-        page = (self.counter//512)*self.columns+column
+        page = column
         position = self.counter%512 #Not needed for now
-        if position == 0:
-            self.counter +=1
-            x0 = Page()
-            self.hold[page] = x0
-            x0.write(value)
-        else:
-            self.counter +=1
-            self.hold[page].write(value)
+        if self.updates == self.columns:
+            self.counter += 1
+        self.updates +=1
+        self.hold[page].write(value)
         return(self.counter)
     def write2(self, value, column, position):
         page = (self.counter//512)*position+column
@@ -97,7 +67,7 @@ class BP:
         return (True)
             
 
-        def read(self, position, column):
+    def read(self, position, column):
         page = ((position//512)-1)*self.columns+column
         schemaPage = ((position//512)-1)*self.columns+4
         indirectionPage = ((position//512)-1)*self.columns+1
