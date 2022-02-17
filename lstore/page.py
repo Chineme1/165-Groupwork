d153e32e2b39589adb54a182e9968c29f3e3f224
KusukabeAsako <86934881+KusukabeAsako@users.noreply.github.com>
2022-02-16 18:37:32 -0800

    Add files via upload

--- lstore/page.py
+++ lstore/page.py
@@ -1,5 +1,6 @@
 
 
+
 class Page:
 
     def __init__(self):
@@ -11,83 +12,38 @@ class Page:
             return False
         else:
             return True
-    def Thas_capacity(self,column): #This is for the Tail Page
-        if self.num_records > 512 - column:
-            return False
-        else:
-            return True
+
     def read(self, position):
-    #I don't need to error check
-        arr =  self.data[position*8 : position*8 +7]
+        position = position-1 #as RID 0 is reserved but we want to use the 0th position of the array
+        arr =  self.data[position*8 : position*8 + 8]
         num = 0
-        num.from_bytes(arr, 'big')
+        num = int.from_bytes(arr, 'big')
         return(num)
 
-    def write(self, value):
+    def write(self, value, position):
         if self.has_capacity():
+            if position == None: #if doesnt give position, add to end
+               position = self.num_records 
+               self.num_records += 1  
+            else:
+                position = position - 1
             arr = value.to_bytes(8, 'big')
-            self.data.extend(arr) # change due to verying size
-            self.num_records += 1
+            self.data[position*8 : position*8+8] = arr 
+            return(position+1) #as RID = position+1
         else:
+            print("page is full")
             return False
-    def write2(self, value, position):
-        if value == None:
-            count = 0
-            while count < 8:
-                self.data[position*8+count] = 0
-                count += 1
-             #self.data[position*8 : position*8+7] = 0 #come back to for changing RID to 0/NULL
-            return(True)
-        arr = value.to_bytes(8, 'big')
-        self.data[position*8 : position*8+7] = arr
-        return(True)
+
+        
+    def pagePrint(self):
+        print(self.data)
 
     
 
-class BP:
-    def __init__(self,columns):
-        self.columns = columns
-        self.hold = []#Physiical holder
-        self.counter = 0
-        self.updates = 0
-        for i in range(0, self.columns+4):
-            x0 = Page()
-            self.hold.append(x0)
-    
-    
-        #def I_PP(self):
-        # if self.has_capacity:
-    
 
-
-    #def has_capacity(self):
-    #   if self.columns == columns
-
-    def write(self,value, column):
-        page = column
-        position = self.counter%512 #Not needed for now
-        if self.updates == self.columns:
-            self.counter += 1
-        self.updates +=1
-        self.hold[page].write(value)
-        return(self.counter)
-    def write2(self, value, column, position):
-        page = (self.counter//512)*position+column
-        position2 = position%512
-        self.hold[page].write2(value, position2)
-        return (True)
-            
-
-    def read(self, position, column):
-        page = ((position//512)-1)*self.columns+column
-        schemaPage = ((position//512)-1)*self.columns+4
-        indirectionPage = ((position//512)-1)*self.columns+1
-        position2 = position%512
-        bit = self.hold[schemaPage].read(position2)%pow(2, 7-column)
-        bit = bit//pow(2, 8-column) 
-        if bit == 1: 
-            encoding = self.hold[indirectionPage].read(position2)
-            return(False, encoding)
-        return(True, self.hold[page].read(position2))#Potentially wrong
-
-
+x0 = Page()
+# for i in range(1, 10):
+    # print(x0.write(i, None))
+# x0.write(5, 1)
+# for i in range(1, 10):
+    # print(x0.read(i))
