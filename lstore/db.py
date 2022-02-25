fb8456f4708a2c16077f86557a55f11dc19e3b79
Yuxin <xyfan@ucdavis.edu>
2022-02-25 03:03:02 -0800

    deleted used parts

--- lstore/db.py
+++ lstore/db.py
@@ -8,8 +8,7 @@ class Database():
         path = None
         pass
 
-    # Not required for milestone1
-    #Optional
+
     def open(self, path):
         self.path = path
         if os.path.isdir(path) == False:
@@ -41,7 +40,7 @@ class Database():
                                     for i in range(0, num_cols-1):
                                         f.readline()
                                     temp.index.indices[0].insert(key, rid, temp.index.indices[0].root)
-    #Optional
+
     def close(self):
         with open(self.path + "/tables" , 'w') as txt_file:
             for i in self.tables:
